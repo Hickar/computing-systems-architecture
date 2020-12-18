f7717025e32d89f4ec6a58b6d373206bd234261a
Egor Pershin <hickar@icloud.com>
2020-12-18 11:57:59 +0300

    Lab3 fix

--- Lab3.asm
+++ Lab3.asm
@@ -1,13 +1,19 @@
 %include "io.inc"
 
 section .data
-Arr times 250 dd -6FFFFFFFh ;Инициализируем массив из 31-разрядных чисел
+Arr times 250 dd -3FFFFFFh;
 average dd 0
 size dw 250
+tmp dw 0
 
 section .text
 global CMAIN
 
+;eax - аккумулятор
+;ebx - указатель на n-ый элемент массива
+;ecx - значение итерации цикла
+;edx - регистр, хранящий значение старших 32 бит при переполнении
+;esi - временно хранит значение, которое в последствии прибавляется/вычитается к/из аккумулятора
 findAverage:
     MOV eax, 0
     MOV ecx, 0
@@ -15,18 +21,18 @@ findAverage:
 findLoop:
     CMP ecx, [size]
     JE calculateAverage
-    ADD eax, [ebx+4*ecx]
-    JO handleOverflow
+    MOV esi, [ebx+4*ecx]
+    CMP esi, 0
+    JNS handlePositive
+handleNegative:
+    NEG esi
+    SUB eax, esi
+    SBB edx, 0
     INC ecx
     JMP findLoop
-handleOverflow:
-    MOV eax, eax
-    JS handlePositiveOverflow
-    SUB edx, 1b
-    INC ecx
-    JMP findLoop
-handlePositiveOverflow:
-    ADD edx, 1b
+handlePositive:
+    ADD eax, esi
+    ADC edx, 0
     INC ecx
     JMP findLoop
 calculateAverage:
@@ -35,6 +41,7 @@ calculateAverage:
     RET
 
 CMAIN:
+    MOV ebp, esp; for correct debugging
     MOV ebx, Arr
     CALL findAverage
     PRINT_STRING "Среднее арифметическое: "
